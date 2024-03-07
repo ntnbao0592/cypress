31865665b380a5a19b210ac70011458e992a4242
bao.nguyen <bao.nguyen@nfq.asia>
2024-03-07 16:37:16 +0700

    init

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,27 @@
+import LoginPage from "../pages/loginPage";
+
+describe("Login test", () => {
+    beforeEach("open login websit", () => {
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // Prevent Cypress from failing the test
+            return false;
+        });
+        cy.visit("login")
+        cy.get(LoginPage.userForm).should("be.visible")
+    })
+
+    it("login correct", () => {
+        cy.get(LoginPage.userNameField).clear().type("ntnbao")
+        cy.get(LoginPage.passwordField).clear().type("b@O11223344")
+        cy.get(LoginPage.loginBtn).click()
+        cy.get(LoginPage.userNameLabel, {timeout: 5000}).should("be.visible")
+    })
+
+    it("login incorrect", () => {
+        cy.get(LoginPage.userNameField).clear().type("ntnbao")
+        cy.get(LoginPage.passwordField).clear().type("b@O11223334")
+        cy.get(LoginPage.loginBtn).click()
+        cy.get(LoginPage.errorMessage).should("be.visible").contains("Invalid username or password!")
+    })
+})
---
+++ cypress/pages/loginPage.ts
@@ -0,0 +1,11 @@
+const LoginPage = {
+    userForm: "#userForm",
+    userNameField: "#userName",
+    passwordField: "#password",
+    loginBtn: "#login",
+    newUserBtn: "//button[text()='New User']",
+    userNameLabel: "#userName-label",
+    passwordLabel: "#password-label",
+    errorMessage: "#name"
+}
+export default LoginPage
