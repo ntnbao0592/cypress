243cde8b9eaf6399925ad900a8d5ffbf09696d56
bao.nguyen <bao.nguyen@nfq.asia>
2024-03-08 09:47:13 +0700

    update

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -22,6 +22,6 @@ describe("Login test", () => {
         cy.get(LoginPage.userNameField).clear().type("ntnbao")
         cy.get(LoginPage.passwordField).clear().type("b@O11223334")
         cy.get(LoginPage.loginBtn).click()
-        cy.get(LoginPage.errorMessage).should("be.visible").contains("Invalid username or password!")
+        cy.get(LoginPage.errorMessage).should("be.visible").contains("Invalid usernames and password!")
     })
 })
